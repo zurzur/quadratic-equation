f0c068adb2a522afdfff1adeac925b56ba1e7ba2
zurzur <@gmail.com>
2018-02-24 07:05:44 +0300

    quadratic-equation/feature: implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  let members = equation.split(" ");
+
+  let a = +members[0];
+  let b = +members[4] * +(members[3]+1);
+  let c = +members[8] * +(members[7]+1);
+
+  let D =  Math.pow(b,2) - 4 * a * c;
+
+  let x1 = (-b + Math.sqrt(D)) / (2*a);
+  let x2 = (-b - Math.sqrt(D)) / (2*a);
+
+  return [x1.toFixed(0), x2.toFixed(0)].sort((x,y)=>x-y);
 }
+
